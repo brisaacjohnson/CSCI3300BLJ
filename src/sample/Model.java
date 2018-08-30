adcb236348b75eb7741311078ce6ccae6e1c5119
brisaacjohnson <brisaacbmw@gmail.com>
2018-08-30 15:15:42 -0400

    Adding default files into my directory

--- src/sample/Model.java
+++ src/sample/Model.java
@@ -1,4 +1,22 @@
 package sample;
 
 public class Model {
+    public String generatePasswordHash(String passwordToHash){
+        String generatedPasswordHash = null;
+        try {
+            MessageDigest messageDiget = MessageDigest.getInstance("MD5");
+            messageDiget.update(passwordToHash.getBytes(StandardCharsets.UTF_8));
+// messageDiget.update(salt.getBytes(StandardCharsets.UTF_8));
+            byte[] bytes = messageDiget.digest();
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++){
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPasswordHash = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        return generatedPasswordHash;
+    }
 }
